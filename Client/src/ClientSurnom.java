5e7573d8e250b937062627baa7e69aa6a489811e
Hakim Bin Luqman <binluqma@polytech.unice.fr>
2013-11-21 15:36:39 +0100

    Ecriture de la méthode main dans le client.

--- Client/src/ClientSurnom.java
+++ Client/src/ClientSurnom.java
@@ -1,8 +1,23 @@
+import java.io.*;
+import java.net.*;
+
 
 public class ClientSurnom {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try(
+			Socket socketSurnom = new Socket("192.168.0.12", 3838);
+			PrintWriter out = new PrintWriter(socketSurnom.getOutputStream(), true);
+	        BufferedReader in = new BufferedReader(
+	        new InputStreamReader(socketSurnom.getInputStream()));) {
+			
+		} catch (UnknownHostException e) {
+			System.err.println("Unable to connect to host 192.168.0.12");
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
